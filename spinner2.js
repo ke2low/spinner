3381a8d2d789762f162dcb45367eec7baeea5978
Kalvin Low <ke2low@uwaterloo.ca>
2019-12-03 00:08:47 +0000

    initial commit, complete

---
+++ spinner2.js
@@ -0,0 +1,30 @@
+
+  // const frames = [process.stdout.write('\r|   ')]
+  const frames = ['\r|   ','\r/   ','\r-   ','\r\\   ','\r|   ','\r/   ','\r-   ','\r\\   ']
+  let delay = 0;
+  for (const char of frames)  {
+    delay+= 400;
+  setTimeout(() => {
+
+    process.stdout.write(char);
+  }, delay)
+  }
+
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\\   '); 
+// }, 700);
+
